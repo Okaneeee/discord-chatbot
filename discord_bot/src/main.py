f1d6faab5bf89ac6f2e4872aaf4a5681b0e82e8e
Anatole <anatole.giniaux@sfr.fr>
2024-02-01 02:17:39 -0500

    feat: main prog of bot

---
+++ discord_bot/src/main.py
@@ -0,0 +1,62 @@
+# -- imports
+## discord
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+## stuff
+# from random import randint
+# import requests
+
+## os & env
+import sys
+from os import getenv
+from dotenv import load_dotenv
+
+# -- config
+load_dotenv()
+
+## const
+TOKEN = getenv('TOKEN')
+GUILD = discord.Object(id=int(getenv('GUILD'))) # type: ignore
+
+PRFX: str = ""
+
+if not TOKEN :
+    sys.exit("Undefined token")
+
+## discord setup
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(PRFX, intents=intents)
+
+print("Bot is starting...")
+
+# -- bot
+@client.event
+async def on_ready():
+    print('We have successfully loggged in as {0.user}'.format(client))
+    sys.stdout.flush()
+    await client.tree.sync(guild=GUILD)
+
+@client.event
+async def on_message(message):
+    # Ignore self
+    if message.author == client.user:
+        return
+    
+    if message.author.id == 430416407554031616 and message.content.lower() == "sync":
+        await message.channel.send("Synced!")
+        return
+
+@client.tree.command(
+    name="ping",
+    description="Replies with pong"
+)
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Pong! ||with *{round(client.latency * 1000)}ms*||")
+
+client.tree.copy_global_to(guild=GUILD)
+
+client.run(TOKEN)
